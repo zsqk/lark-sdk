47c02d7f24b01676e6a19a680dce69f26a40d832
iugo <iugogogo@gmail.com>
2023-04-03 13:59:20 +0800

    rf: 调整代码结构

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const LARK_SERVER_ENDPOINT = 'https://open.feishu.cn/open-apis/auth/v3';
--- src/mod.ts
+++ src/mod.ts
@@ -1,31 +1,4 @@
-const LARK_SERVER_ENDPOINT = 'https://open.feishu.cn/open-apis/auth/v3';
-
-// TODO: 将请求通用化, 并统一处理错误
-
-/**
- * [doc](https://open.feishu.cn/document/ukTMukTMukTM/ukDNz4SO0MjL5QzM/auth-v3/auth/app_access_token_internal)
- */
-async function getAppAccessToken(opt: {
-  'app_id': string;
-  'app_secret': string;
-}): Promise<{
-  'code': number;
-  'msg': string;
-  'app_access_token': string;
-  'expire': number;
-}> {
-  const url = new URL(
-    `${LARK_SERVER_ENDPOINT}/app_access_token/internal`,
-  );
-  const res = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(opt),
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-  }).then((res) => res.json());
-  return res;
-}
+import { getAppAccessToken } from './auth/gettoken.ts';
 
 /**
  * Lark/飞书 自建应用 SDK
